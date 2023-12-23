6e89fcf44e3f1d30972d364fae13d1acb035383c
Brittany Babbitt <britt@Brittanys-MacBook-Pro.local>
2023-12-23 17:51:50 -0500

    added all as default filter string

--- jobs-monthly/src/app/services/jobs-api.service.ts
+++ jobs-monthly/src/app/services/jobs-api.service.ts
@@ -1,10 +1,18 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
+import { Observable } from 'rxjs';
+
+import { JobDescription } from '../interfaces/job-description.object';
+
 @Injectable({
   providedIn: 'root'
 })
 export class JobsApiService {
 
   constructor(private http: HttpClient) { }
+
+  getJobDescriptions(filter: string = 'all'): Observable<JobDescription> {
+    return this.http.get<JobDescription>(`/api/user/${filter}`);
+  }
 }
