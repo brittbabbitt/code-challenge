32f69605d797f6ef9cc0a533788e086d6ffaf42a
Brittany Babbitt <britt@Brittanys-MacBook-Pro.local>
2023-12-23 17:55:29 -0500

    scaffold job-container component

---
+++ jobs-monthly/src/app/components/job-container/job-container.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-job-container',
+  standalone: true,
+  imports: [],
+  templateUrl: './job-container.component.html',
+  styleUrl: './job-container.component.scss'
+})
+export class JobContainerComponent {
+
+}
