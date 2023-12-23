98dced08124c339f8a682259afa866be13cc18b6
Brittany Babbitt <britt@Brittanys-MacBook-Pro.local>
2023-12-23 16:55:31 -0500

    scaffold job-table component

---
+++ jobs-monthly/src/app/components/job-table/job-table.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-job-table',
+  standalone: true,
+  imports: [],
+  templateUrl: './job-table.component.html'
+})
+export class JobTableComponent {
+
+}
