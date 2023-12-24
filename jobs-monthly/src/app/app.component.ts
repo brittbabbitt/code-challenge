1d35886f14d770bf4a094ddd66932e6ff61cf6b5
Brittany Babbitt <britt@Brittanys-MacBook-Pro.local>
2023-12-23 19:38:10 -0500

    updated app component imports

--- jobs-monthly/src/app/app.component.ts
+++ jobs-monthly/src/app/app.component.ts
@@ -2,10 +2,17 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+import {
+  JobContainerComponent,
+} from '@components/job-container/job-container.component';
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [
+    CommonModule,
+    JobContainerComponent,
+    RouterOutlet],
   templateUrl: './app.component.html'
 })
 export class AppComponent {
