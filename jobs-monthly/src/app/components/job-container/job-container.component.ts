c3522db40a8fe88c9c7ee6b6cd751b8de77ff467
Brittany Babbitt <britt@Brittanys-MacBook-Pro.local>
2023-12-23 19:40:57 -0500

    added jobService subscription to job-container component

--- jobs-monthly/src/app/components/job-container/job-container.component.ts
+++ jobs-monthly/src/app/components/job-container/job-container.component.ts
@@ -1,12 +1,35 @@
 import { Component } from '@angular/core';
 
+import {
+  catchError,
+  take,
+  tap,
+} from 'rxjs';
+
+import { JobsApiService } from '@services/jobs-api.service';
+
 @Component({
   selector: 'app-job-container',
   standalone: true,
   imports: [],
-  templateUrl: './job-container.component.html',
-  styleUrl: './job-container.component.scss'
+  templateUrl: './job-container.component.html'
 })
 export class JobContainerComponent {
 
+  constructor(private jobService: JobsApiService ) { }
+
+  getJobDescriptions(filter: string = 'all') {
+    this.jobService.getJobDescriptions(filter).pipe(
+      take(1),
+      tap((data) => data),
+      catchError((error) => { throw `Error Occurred: ${error}` })
+    ).subscribe({
+      next: (data) => console.log(data),
+      error: (error) => console.log(error)
+    })
+  }
+
+  ngOnit(){
+    this.getJobDescriptions();
+  }
 }
