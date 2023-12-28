54ffb204940f01f1a30fe8a850d18732c9578e12
Brittany Babbitt <britt@Brittanys-MacBook-Pro.local>
2023-12-28 11:56:20 -0500

    moved all interfaces into job-description for readability

--- jobs-monthly/src/app/interfaces/job-description.object.ts
+++ jobs-monthly/src/app/interfaces/job-description.object.ts
@@ -1,10 +1,19 @@
-import { JobMonthly } from './job-monthly.object';
-
 export interface JobDescription {
   websiteTitle: string,
   websiteOrganization: string,
   websiteLocation: string,
   websiteDatePublished: string
+}
+
+export interface JobDescriptionData {
+  data: {
+    jobDescriptions: JobDescription[]
+  }
+}
+
+export interface JobMonthly {
+  month: string,
+  jobs: JobDescription[],
 }
 
 export interface JobDescriptionState {
@@ -14,8 +23,17 @@ export interface JobDescriptionState {
   apiError: string | null;
 };
 
-export interface JobDescriptionData {
-  data: {
-    jobDescriptions: JobDescription[]
-  }
+export enum MONTHS {
+  JAN = '01',
+  FEB = '02',
+  MAR = '03',
+  APR = '04',
+  MAY = '05',
+  JUN = '06',
+  JUL = '07',
+  AUG = '08',
+  SEP = '09',
+  OCT = '10',
+  NOV = '11',
+  DEC = '12'
 }
