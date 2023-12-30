e6ab55fd2865bd75f58a8635026b9d0e989995f2
Brittany Babbitt <britt@Brittanys-MacBook-Pro.local>
2023-12-30 14:28:57 -0500

    added job-container tests

--- jobs-monthly/src/app/components/job-container/job-container.component.spec.ts
+++ jobs-monthly/src/app/components/job-container/job-container.component.spec.ts
@@ -4,38 +4,77 @@ import {
   TestBed,
 } from '@angular/core/testing';
 
-import { JobsApiService } from '@services/jobs-api.service';
 import { JobsStore } from '@stores/jobs-store.store';
 
 import { JobContainerComponent } from './job-container.component';
 
 describe('JobContainerComponent', () => {
-  const MockJobsStoreSpy = jasmine.createSpyObj('JobsStore', [
-    'getJobDescriptions',
-    'setNewJobsByMonth',
-  ]);
 
-  const MockJobsApiService = jasmine.createSpyObj('JobsApiService', ['getJobDescriptions']);
+  const TESTDATA = {
+    jobsByMonth: {
+      month: 'JAN',
+      jobs: [
+        {
+          "websiteTitle": "CERTARA",
+          "websiteOrganization": "Certara, USA",
+          "websiteLocation": "www.Certara.com",
+          "websiteDatePublished": "2023-01-17T02:00:30Z"
+        },
+      ]
+    },
+    jobDescriptions: [
+      {
+        "websiteTitle": "CERTARA",
+        "websiteOrganization": "Certara, USA",
+        "websiteLocation": "www.Certara.com",
+        "websiteDatePublished": "2023-01-17T02:00:30Z"
+      },
+      {
+        "websiteTitle": "GOOGLE",
+        "websiteOrganization": "ABC, Inc.",
+        "websiteLocation": "www.Google.com",
+        "websiteDatePublished": "2023-12-05T01:38:29Z"
+      },
+    ]
+  }
 
   let component: JobContainerComponent;
   let fixture: ComponentFixture<JobContainerComponent>;
+  let store: JobsStore;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [JobContainerComponent, HttpClientModule],
-      providers: [
-        {provider: JobsStore, useValue: MockJobsStoreSpy},
-        {provider: JobsApiService, useValue: MockJobsApiService},
-      ]
+      providers: [JobsStore]
     })
     .compileComponents();
+  });
 
+  beforeEach( () => {
     fixture = TestBed.createComponent(JobContainerComponent);
+    store = fixture.debugElement.injector.get(JobsStore);
     component = fixture.componentInstance;
     fixture.detectChanges();
-  });
+  })
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  describe('setNewJobsByMonth', () => {
+
+    it('should call setNewJobsByMonth with params only once', () => {
+      const compSpy = spyOn(component, 'setNewJobsByMonth');
+      component.setNewJobsByMonth('JAN');
+      expect(compSpy).toHaveBeenCalledOnceWith('JAN');
+    })
+
+    it('should call the JobsStore.setNewJobsByMonth', () => {
+      const storeSpy = spyOn(store, 'setNewJobsByMonth');
+      component.setNewJobsByMonth('JAN');
+      expect(storeSpy).toHaveBeenCalled();
+    })
+
+  });
+
 });
