9781448546e3522ae0eacbea6ef034c3c48bb7ce
Brittany Babbitt <britt@Brittanys-MacBook-Pro.local>
2023-12-30 20:02:43 -0500

    moving mockData for component testing to separate file

--- jobs-monthly/src/app/components/job-container/job-container.component.spec.ts
+++ jobs-monthly/src/app/components/job-container/job-container.component.spec.ts
@@ -10,34 +10,6 @@ import { JobContainerComponent } from './job-container.component';
 
 describe('JobContainerComponent', () => {
 
-  const TESTDATA = {
-    jobsByMonth: {
-      month: 'JAN',
-      jobs: [
-        {
-          "websiteTitle": "CERTARA",
-          "websiteOrganization": "Certara, USA",
-          "websiteLocation": "www.Certara.com",
-          "websiteDatePublished": "2023-01-17T02:00:30Z"
-        },
-      ]
-    },
-    jobDescriptions: [
-      {
-        "websiteTitle": "CERTARA",
-        "websiteOrganization": "Certara, USA",
-        "websiteLocation": "www.Certara.com",
-        "websiteDatePublished": "2023-01-17T02:00:30Z"
-      },
-      {
-        "websiteTitle": "GOOGLE",
-        "websiteOrganization": "ABC, Inc.",
-        "websiteLocation": "www.Google.com",
-        "websiteDatePublished": "2023-12-05T01:38:29Z"
-      },
-    ]
-  }
-
   let component: JobContainerComponent;
   let fixture: ComponentFixture<JobContainerComponent>;
   let store: JobsStore;
---
+++ jobs-monthly/src/app/components/mockData.test.ts
@@ -0,0 +1,29 @@
+const TESTDATA = {
+  jobsByMonth: {
+    month: 'JAN',
+    jobs: [
+      {
+        "websiteTitle": "CERTARA",
+        "websiteOrganization": "Certara, USA",
+        "websiteLocation": "www.Certara.com",
+        "websiteDatePublished": "2023-01-17T02:00:30Z"
+      },
+    ]
+  },
+  jobDescriptions: [
+    {
+      "websiteTitle": "CERTARA",
+      "websiteOrganization": "Certara, USA",
+      "websiteLocation": "www.Certara.com",
+      "websiteDatePublished": "2023-01-17T02:00:30Z"
+    },
+    {
+      "websiteTitle": "GOOGLE",
+      "websiteOrganization": "ABC, Inc.",
+      "websiteLocation": "www.Google.com",
+      "websiteDatePublished": "2023-12-05T01:38:29Z"
+    },
+  ]
+}
+
+export default TESTDATA;
